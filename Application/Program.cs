7731971c502a7684f3c7692e6fbfbf96e44a99e8
Matt_Condon <matt_condon@dell.com>
2020-09-13 16:05:32 +0100

    Import fpl client

---
+++ Application/Program.cs
@@ -0,0 +1,31 @@
+﻿using FplClient.Clients;
+using FplClient.Data;
+using FPLTeamManager.Infrastructure.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FPLTeamManager.Application
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var players = await GetTopPlayersAsync();
+            foreach (var player in players)
+            {
+                Console.WriteLine($"{player.GetPartialPlayerString()}");
+            }
+        }
+
+        private static async Task<IEnumerable<FplPlayer>> GetTopPlayersAsync()
+        {
+            var client = new FplPlayerClient(new HttpClient());
+            var players = await client.GetAllPlayers();
+            Console.WriteLine(players.Count);
+            return players.OrderByDescending(x => x.TotalPoints).Take(10);
+        }
+    }
+}
