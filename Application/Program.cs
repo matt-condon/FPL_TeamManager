a17ad189120909de33b610199a5496006da5834e
Matt_Condon <matt_condon@dell.com>
2020-09-14 02:10:17 +0100

    Implementing initial player evaluation service (by transfers and ownership)

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,6 +1,7 @@
 ﻿using FplClient.Clients;
 using FplClient.Data;
 using FPLTeamManager.Application.Builders;
+using FPLTeamManager.Application.Services;
 using FPLTeamManager.Infrastructure.Extensions;
 using System;
 using System.Collections.Generic;
@@ -17,19 +18,24 @@ namespace FPLTeamManager.Application
             var players = await GetTopPlayersAsync();
 
             var dictionaryBuilder = new PlayerDictionaryBuilder();
+            var playerEvaluationService = new PlayerEvaluationService();
+
             var playerDictionary = dictionaryBuilder.BuildFilteredPlayerDictionary(players);
 
-
             Console.WriteLine($"Number of players (after filter): {CountNumberOfPlayers(playerDictionary)}");
+
 
             foreach (var position in playerDictionary)
             {
                 Console.WriteLine($"Position: {position.Key}");
                 Console.WriteLine("");
+                var orderedPlayers = position.Value.OrderByDescending(x => playerEvaluationService.EvaluatePlayer(x)).Take(10);
 
-                foreach (var player in position.Value)
+                foreach (var player in orderedPlayers)
                 {
                     Console.WriteLine($"{player.GetPartialPlayerString()}");
+                    var playerVal = playerEvaluationService.EvaluatePlayer(player);
+                    Console.WriteLine($"Player Evaluation: {playerVal}");
                 }
                 Console.WriteLine("");
             }
@@ -39,8 +45,7 @@ namespace FPLTeamManager.Application
         {
             var client = new FplPlayerClient(new HttpClient());
             var players = await client.GetAllPlayers();
-            Console.WriteLine($"Number of players: {players.Count}");
-            return players.OrderByDescending(x => x.TotalPoints).Take(12);
+            return players;
         }
 
         private static int CountNumberOfPlayers(Dictionary<FplPlayerPosition, List<FplPlayer>> playerDictionary)
---
+++ Application/Services/PlayerEvaluationService.cs
@@ -0,0 +1,21 @@
+﻿using FplClient.Data;
+
+namespace FPLTeamManager.Application.Services
+{
+    public class PlayerEvaluationService
+    {
+        public double EvaluatePlayer(FplPlayer player)
+        {
+            var evaluation = player.PointsPerGame + player.ValueSeason;
+            var playerValue = player.NowCost / 10.0;
+            var transferDifferential = (player.TransfersInEvent > player.TransfersOutEvent) ? (1 - (1 / EvaluateTransferDifferential(player))) : (EvaluateTransferDifferential(player) - 1);
+            var ownerMultiplier = 1 - 1 / player.OwnershipPercentage;
+            return (transferDifferential * 0.75) + (ownerMultiplier * 0.25);
+        }
+
+        private double EvaluateTransferDifferential(FplPlayer player)
+        {
+            return (double)(player.TransfersInEvent + 1) / (player.TransfersOutEvent + 1);
+        }
+    }
+}
