6f0dae4ba9cd0f784b20ce6cef6d04dfce513dd4
Matt_Condon <matt_condon@dell.com>
2020-09-17 22:28:20 +0100

    chore: removing moved namespaces

--- Infrastructure/Extensions/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace FPLTeamManager.Infrastructure.Extensions
-{
-    public static class StringExtensions
-    {
-        public static string ConcatWithNewLine(this string str, string concatString) => $"{str}{concatString}{Environment.NewLine}";
-    }
-}
