3f420ff5172cdf256a66932bcbd937b28a49d945
Matt_Condon <matt_condon@dell.com>
2021-08-03 01:35:35 +0100

    tech: add app configs for account management and authentication

--- src/FplManager/Application/Program.cs
+++ src/FplManager/Application/Program.cs
@@ -1,77 +1,73 @@
-﻿using FplClient.Clients;
-using FplClient.Data;
-using FplManager.Application.Builders;
-using FplManager.Application.Services;
-using FplManager.Infrastructure.Extensions;
-using FplManager.Infrastructure.FplClients;
+﻿using FplManager.Application.Services;
+using FplManager.Configuration;
 using FplManager.Infrastructure.Models;
-using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FplManager.Application
 {
-    class Program
+    public class Program
     {
         private static TeamOrchestratorService _teamOrchestratorService;
 
         static async Task Main(string[] args)
         {
-            CookieContainer cookieContainer = new CookieContainer();
-            HttpClientHandler handler = new HttpClientHandler();
-            handler.CookieContainer = cookieContainer;
-            var httpClient = new HttpClient(handler);
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("./Configuration/appSettings.json", true, true);
+            var config = builder.Build();
+            var appConfig = config.Get<AppConfig>();
+
+            var accounts = appConfig.AccountModel;
+            var httpClient = new HttpClient();
 
             _teamOrchestratorService = new TeamOrchestratorService(httpClient);
 
-            //var account = "fijife@gmail.com";
-            //var fplTeamId = 1445605;
+            foreach (var account in accounts)
+            {
+                var authConfig = account.AuthModel;
+                var authConfigAsDictionary = GetValues(authConfig);
+                await AuthenticateAsync(httpClient, appConfig.LogInUrl, authConfigAsDictionary);
 
-            //var account = "bobbsbobbs@gmail.com";
-            //var fplTeamId = 1223804;
+                await _teamOrchestratorService.ManageTeam(account.FplTeamId, numberOfTransfers: 0);
+            }
+        }
 
-            //var account = "matt.condon@ucdconnect.ie";
-            //var fplTeamId = 1224412;
-
-            var account = "mattoconduin@gmail.com";
-            var fplTeamId = 1828488;
-
-            await AuthenticateAsync(httpClient, account, "PASSWORD_HERE");
-
-            await _teamOrchestratorService.ManageTeam(fplTeamId, numberOfTransfers: 0);
+        public static Dictionary<string, string> GetValues(AuthenticationModel obj) 
+        {
+            return obj
+                    .GetType()
+                    .GetProperties()
+                    .ToDictionary(p=>p.Name, p=> p.GetValue(obj).ToString());
         }
 
         //You need to authenticate, then you need the pl_profile cookie for every subsequent request
-        public static async Task AuthenticateAsync(HttpClient httpClient, string login, string password)
+        public static async Task AuthenticateAsync(HttpClient httpClient, string loginUrl, Dictionary<string, string> auth)
         {
             try
             {
-                var nvc = new List<KeyValuePair<string, string>>
-                {
-                    new KeyValuePair<string, string>("login", login),
-                    new KeyValuePair<string, string>("password", password),
-                    new KeyValuePair<string, string>("app", "plfpl-web"),
-                    new KeyValuePair<string, string>("redirect_uri", "https://fantasy.premierleague.com/")
-                };
-
-                var uri = new Uri("https://users.premierleague.com/accounts/login/");
+                var uri = new Uri(loginUrl);
                 var httpRequest = new HttpRequestMessage
                 {
                     RequestUri = uri,
                     Method = HttpMethod.Post,
-                    Content = new FormUrlEncodedContent(nvc)
+                    Content = new FormUrlEncodedContent(auth)
                 };
 
-                await httpClient.SendAsync(httpRequest);
+                var loginResponse = await httpClient.SendAsync(httpRequest);
+
+                if (loginResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    Console.WriteLine($"Login Failed. StatusCode: {loginResponse.StatusCode}");
+                }
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Login failed for account: {login}, error: {e}");
+                Console.WriteLine($"Login failed, error: {e}");
             }
         }
     }
---
+++ src/FplManager/Infrastructure/Models/AccountModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FplManager.Infrastructure.Models
+{
+    public class AccountModel
+    {
+        public int FplTeamId { get; set; }
+        public AuthenticationModel AuthModel { get; set; }
+    }
+
+    public class AuthenticationModel
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public string app { get; set; }
+        public string redirect_uri { get; set; }
+    }
+}
