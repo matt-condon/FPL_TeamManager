60fd6a5582e20b11af12fec5b0d05a29db46cdf9
Matt_Condon <matt_condon@dell.com>
2021-08-03 01:35:35 +0100

    tech: add app configs for account management and authentication

---
+++ src/FplManager/Configuration/AppConfig.cs
@@ -0,0 +1,10 @@
+﻿using FplManager.Infrastructure.Models;
+
+namespace FplManager.Configuration
+{
+    public class AppConfig
+    {
+        public AccountModel[] AccountModel { get; set; }
+        public string LogInUrl { get; set; }
+    }
+}
