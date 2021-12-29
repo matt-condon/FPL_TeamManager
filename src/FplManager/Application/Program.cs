71f7d00bae1a881a933118956da828c5785bad7d
matt-condon <matt_condon@dell.com>
2021-12-29 17:54:44 +0000

    tech: register as hosted service

--- src/FplManager/Application/Program.cs
+++ src/FplManager/Application/Program.cs
@@ -1,82 +1,38 @@
 ﻿using FplManager.Application.Services;
 using FplManager.Configuration;
-using FplManager.Infrastructure.Models;
+using FplManager.HostedServices;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 
 namespace FplManager.Application
 {
     public class Program
     {
-        private const bool TestOnly = false;
-        private const int NumberOfTransfers = 1;
-        private const bool RequireTransferApproval = true;
-        private const bool FreeTransfersOnly = true;
-        private const bool UseWC = false;
-
-        private static TeamOrchestratorService _teamOrchestratorService;
-
         static async Task Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("./Configuration/appSettings.json", true, true);
-            var config = builder.Build();
-            var appConfig = config.Get<AppConfig>();
-            var accounts = appConfig.AccountModel;
-            
-            accounts = TestOnly ? accounts.Take(1) : accounts;
-
-            var httpClient = new HttpClient();
-
-            _teamOrchestratorService = new TeamOrchestratorService(httpClient);
-
-            foreach (var account in accounts)
-            {
-                var authConfig = account.AuthModel;
-                var authConfigAsDictionary = GetValues(authConfig);
-                await AuthenticateAsync(httpClient, appConfig.LogInUrl, authConfigAsDictionary);
-
-                await _teamOrchestratorService.ManageTeam(account.FplTeamId, account.TransferPercentile , NumberOfTransfers, RequireTransferApproval, FreeTransfersOnly, UseWC);
-            }
+            using var host = CreateHostBuilder(args).Build();
+            host.Run();
         }
 
-        public static Dictionary<string, string> GetValues(AuthenticationModel obj) 
-        {
-            return obj
-                    .GetType()
-                    .GetProperties()
-                    .ToDictionary(p=>p.Name, p=> p.GetValue(obj).ToString());
-        }
-
-        //You need to authenticate, then you need the pl_profile cookie for every subsequent request
-        public static async Task AuthenticateAsync(HttpClient httpClient, string loginUrl, Dictionary<string, string> auth)
-        {
-            try
-            {
-                var uri = new Uri(loginUrl);
-                var httpRequest = new HttpRequestMessage
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((builderContext, config) =>
                 {
-                    RequestUri = uri,
-                    Method = HttpMethod.Post,
-                    Content = new FormUrlEncodedContent(auth)
-                };
-
-                var loginResponse = await httpClient.SendAsync(httpRequest);
-
-                if (loginResponse.StatusCode != HttpStatusCode.OK)
+                    config.AddJsonFile($"Configuration/appsettings.json", false)
+                   .AddEnvironmentVariables();
+                })
+                .ConfigureServices((builderContext, services) =>
                 {
-                    Console.WriteLine($"Login Failed. StatusCode: {loginResponse.StatusCode}");
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Login failed, error: {e}");
-            }
-        }
+                    services.AddOptions();
+                    services.AddHttpClient();
+
+                    services.Configure<AppConfig>(builderContext.Configuration);
+
+                    services.AddSingleton<IApplication, Application>();
+                    services.AddSingleton<ITeamOrchestratorService, TeamOrchestratorService>();
+                    services.AddHostedService<FplTeamManager>();
+                });
     }
 }
---
+++ src/FplManager/HostedServices/FplTeamManager.cs
@@ -0,0 +1,30 @@
+﻿using FplManager.Application;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FplManager.HostedServices
+{
+    public class FplTeamManager : IHostedService
+    {
+        readonly IApplication _app;
+        readonly ILogger<FplTeamManager> _logger;
+        public FplTeamManager(ILogger<FplTeamManager> logger, IApplication app)
+        {
+            _app = app;
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _app.Run();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogWarning("Application has ended");
+            return Task.CompletedTask;
+        }
+    }
+}
